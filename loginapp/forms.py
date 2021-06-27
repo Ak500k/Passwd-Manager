f51506ae428c1836d3395d481706a2db07aea16f
Ashish <ashishdungdung6@gmail.com>
2021-06-27 17:42:19 +0530

    Added account page

--- loginapp/forms.py
+++ loginapp/forms.py
@@ -1,5 +1,8 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import validators
 from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
 from loginapp.models import User
 
@@ -68,3 +71,17 @@ class ResetPasswordForm(FlaskForm):
     confirm_password = PasswordField('Confirm password', validators=[DataRequired(), EqualTo('password')])
 
     submit_btn = SubmitField('Reset Password')
+
+
+class UserAccountUpdate(FlaskForm):    
+    name = StringField('Name', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit_btn = SubmitField('Update')
+
+    # Adding validation for abnormal anomalies
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            test_condition = User.query.filter_by(email = email.data).first()
+            if test_condition:
+                raise ValidationError('Email already exist in database.')
