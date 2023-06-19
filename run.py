cb1c8a71bd8cb2f3f88e3021ca13aa10fcfc3f5c
djit20 <136679188+djit20@users.noreply.github.com>
2023-06-19 11:49:10 +0530

    Update run.py

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from loginapp import app, db
 
 if __name__ == '__main__':
     db.create_all() # first we need to create table other wise HUGE ERROR. Wasted-24hr :(
-    app.run(debug=True, port=8000)
+    app.run(debug=True, port=80)
