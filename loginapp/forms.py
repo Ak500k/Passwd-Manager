4d80b95732940363d9365b090ff7de5ecd2c52fa
Gousios Evangelos Alexantar <egousios@csd.auth.gr>
2024-02-18 00:01:08 +0200

    'DRY', consistent naming, clearer comments, improved error messages, Flask-Login validation, and added password length validation.

--- loginapp/forms.py
+++ loginapp/forms.py
@@ -2,95 +2,63 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms import validators
 from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
 from loginapp.models import User
 
+email_validator = [DataRequired(), Email()]
+
 class RegistrationForm(FlaskForm):
-    """
-    Registration form 
-    Elements containing:
-    1. name
-    2. email
-    3. password
-    4. confirm password
-    5. submit button
-    """
     name = StringField('Name', validators=[DataRequired(), Length(min=2, max=20)])
-    
-    email = StringField('Email', validators=[DataRequired(), Email()])
-
-    password = PasswordField('Password', validators=[DataRequired()])
-
+    email = StringField('Email', validators=email_validator)
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
     confirm_password = PasswordField('Confirm password', validators=[DataRequired(), EqualTo('password')])
+    submit_button = SubmitField('Register')
 
-    submit_btn = SubmitField('Register')
-
-    # Adding validation for abnormal anomalies
     def validate_email(self, email):
-        test_condition = User.query.filter_by(email = email.data).first()
-        if test_condition:
-            raise ValidationError('Email already exist in database.')
+        if User.query.filter_by(email=email.data).first():
+            raise ValidationError('Email already exists in the database.')
 
-class LoginForm(FlaskForm):    
-    """
-    Login form 
-    Elements containing:
-    1. email
-    2. password
-    3. submit button
-    """
-    email = StringField('Email', validators=[DataRequired(), Email()])
-
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=email_validator)
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember me')
-    submit_btn = SubmitField('Login')
-
+    submit_button = SubmitField('Login')
 
 class AddPassword(FlaskForm):
-    """ Form for add page. Where user will input therir credintial in password manager. """
     webaddress = StringField('Web Address', validators=[DataRequired()])
     username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    email = StringField('Email', validators=email_validator)
     password = PasswordField('Password', validators=[DataRequired()])
+    submit_button = SubmitField('Submit')
 
-    submit_btn = SubmitField('Submit')
-
-# password reset form
 class RequestResetForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    submit_btn = SubmitField('Request Password Reset')
+    email = StringField('Email', validators=email_validator)
+    submit_button = SubmitField('Request Password Reset')
 
     def validate_email(self, email):
-        user = User.query.filter_by(email = email.data).first()
+        user = User.query.filter_by(email=email.data).first()
         if user is None:
             raise ValidationError('There is no account with this email. You must register first.')
 
 class ResetPasswordForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm password', validators=[DataRequired(), EqualTo('password')])
+    submit_button = SubmitField('Reset Password')
 
-    submit_btn = SubmitField('Reset Password')
-
-
-class UserAccountUpdate(FlaskForm):    
+class UserAccountUpdate(FlaskForm):
     name = StringField('Name', validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    email = StringField('Email', validators=email_validator)
     picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
-    submit_btn = SubmitField('Update')
+    submit_button = SubmitField('Update')
 
-    # Adding validation for abnormal anomalies
     def validate_email(self, email):
         if email.data != current_user.email:
-            test_condition = User.query.filter_by(email = email.data).first()
-            if test_condition:
-                raise ValidationError('Email already exist in database.')
+            if User.query.filter_by(email=email.data).first():
+                raise ValidationError('Email already exists in the database.')
 
 class UpdatePassword(FlaskForm):
-    """ Form for Update page. Where user will update their credintial in password manager. """
     webaddress = StringField('Web Address', validators=[DataRequired()])
     username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    email = StringField('Email', validators=email_validator)
     password = PasswordField('Password', validators=[DataRequired()])
-
-    submit_btn = SubmitField('Update')
+    submit_button = SubmitField('Update')
